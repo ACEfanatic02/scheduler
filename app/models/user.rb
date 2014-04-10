e2bc20cbce65bea7c5f71ddab2c07b20f6560ca3
ACEfanatic02 <acefanatic02@gmail.com>
2014-04-09 22:47:09 -0400

    Added secure password to User model.
    Requires a minimum password length of 8 characters, no maximum.  Also
    added enfored uniqueness on email addresses.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
   # "Proper' regex is a loser's game here.
   # Just check for something vaugely email-shaped.
   VALID_EMAIL_REGEX = /.+@.+\..+/
 
   validates :username, presence: true, length: { maximum: 50 } 
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, 
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, length: { minimum: 8 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,18 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(username: "test1234", email: "test1234@example.com") }
+  before { @user = User.new(username: "test1234", email: "test1234@example.com",
+                            password: "foobarbaz", password_confirmation: "foobarbaz") }
 
   subject { @user }
 
   it { should respond_to(:username) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -45,5 +51,49 @@ describe User do
         expect(@user).to be_valid
       end
     end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(username: "example", email: "user@example.com", 
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalidpassword") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_falsey }
+    end
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 7 }
+    it { should be_invalid }
   end
 end
