31bd2f9c416b32ae0132e739596ce04424b6bc0d
ACEfanatic02 <acefanatic02@gmail.com>
2014-04-10 23:08:39 -0400

    Begin acceptance testing of registration form.
    Checking JS validation of the registration form before submission.
    (So that we can display errors immediately without a round-trip to
    the server.)

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ end
 group :development, :test do
   gem 'rspec-rails', '~> 3.0.0.beta'
   gem 'capybara', '2.2.1'
+  gem 'selenium-webdriver'
 end
 
 # Use SCSS for stylesheets
--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -16,5 +16,16 @@ describe "User pages" do
     it { should have_field('password_confirmation') }
 
     it { should have_css('form button', text: "Register") }
+    
+    describe "registration form validation", js: true do
+
+      it "marks empty fields as errors" do
+        click_button('Register')
+        expect(page).to have_selector('input[name=username].form-field-error')
+        expect(page).to have_selector('input[name=email].form-field-error')
+        expect(page).to have_selector('input[name=password].form-field-error')
+        expect(page).to have_selector('input[name=password_confirmation].form-field-error')
+      end
+    end
   end
 end
