5f95d2b9aee3df18f34b743c28f8d5d278d9f02c
ACEfanatic02 <acefanatic02@gmail.com>
2014-04-10 22:20:41 -0400

    Content specs for registration page.
    Next up:  client-side validation.

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -8,5 +8,13 @@ describe "User pages" do
     before { visit register_path }
 
     it { should have_title('Register') }
+    it { should have_content('New User') }
+
+    it { should have_field('username') }
+    it { should have_field('email') }
+    it { should have_field('password') }
+    it { should have_field('password_confirmation') }
+
+    it { should have_css('form button', text: "Register") }
   end
 end
