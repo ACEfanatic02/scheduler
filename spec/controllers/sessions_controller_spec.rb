c83f07d616d9af20b1a2aaf832eb50d0c1d5d3e7
ACEfanatic02 <acefanatic02@gmail.com>
2014-04-12 22:13:09 -0400

    Session controller tests.
    Automated tests for login/logout.  Apparently controller tests have
    access to the session, even if integration tests do not.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe SessionsController do
+  before do
+    @user = User.new(username: 'user', email: 'user@example.com',
+                     password: 'password', password_confirmation: 'password')
+    @user.save!
+  end
 
+  describe '#create' do
+    it 'logs in user with correct credentials' do
+      post :create, email: 'user@example.com', password: 'password'
+      expect(session[:user_id]).to eq(@user.id)
+    end
+
+    it 'does not log in user with incorrect credentials' do
+      post :create, email: 'user@example.com', password: 'wrongpassword'
+      expect(session[:user_id]).not_to eq(@user.id)
+    end
+  end
+
+  describe '#destroy' do
+    before do
+      post :create, email: 'user@example.com', password: 'password'
+    end
+
+    it 'logs out current user' do
+      delete :destroy
+      expect(session[:user_id]).not_to eq(@user.id)
+    end
+  end
 end
