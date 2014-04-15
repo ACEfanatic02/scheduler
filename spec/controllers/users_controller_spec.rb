7a158b1574fa9e095a6b3d1332c935723c5d914d
ACEfanatic02 <acefanatic02@gmail.com>
2014-04-15 19:28:52 -0400

    Refactor user controller specs.
    Group the valid / invalid tests, factor out common setup.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,24 +4,36 @@ describe UsersController do
 
   describe '#create' do
 
-    it "redirects to root page for a valid user" do
-      post :create, { username: 'user', email: 'user@example.com', 
-                      password: 'password', password_confirmation: 'password' }
-      expect(flash[:success]).not_to be_nil
-      expect(response).to redirect_to root_url
+    describe "with valid user data" do
+      before do
+        post :create, { username: 'user', email: 'user@example.com', 
+                        password: 'password', password_confirmation: 'password' }
+      end
+
+      it "redirects to root page" do
+        expect(flash[:success]).not_to be_nil
+        expect(response).to redirect_to root_url
+      end
+
+      it "logs in the new user" do
+        created_user = User.find_by_email('user@example.com')
+        expect(session[:user_id]).to eq(created_user.id)
+      end
     end
 
-    it "logs in newly created user" do
-      post :create, { username: 'user', email: 'user@example.com', 
-                      password: 'password', password_confirmation: 'password' }
-      created_user = User.find_by_email('user@example.com')
-      expect(session[:user_id]).to eq(created_user.id)
-    end
+    describe "with invalid user data" do
+      before do
+        post :create, { username: 'user', email: '', password: '', password_confirmation: ' ' }
+      end
 
-    it "re-renders the form for an invalid user" do
-      post :create, { username: 'user', email: '', password: '', password_confirmation: ' ' }
-      expect(flash[:success]).to be_nil
-      expect(response).to render_template(:new)
+      it "re-renders the registration form" do
+        expect(flash[:success]).to be_nil
+        expect(response).to render_template(:new)
+      end
+
+      it "does not log in" do
+        expect(session[:user_id]).to be_nil
+      end
     end
   end
 end
