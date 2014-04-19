05540b0e431db530571a4c4538abfc74d2007fe8
ACEfanatic02 <acefanatic02@gmail.com>
2014-04-19 18:04:48 -0400

    More schedule work.
    Fix some sizing issues: `rem` vs. `em`.
    Fleshing out display tables.

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,8 +1,9 @@
 class ScheduleController < ApplicationController
 
   def index
-    @start_date = params[:start_date].to_datetime
-    @end_date = @start_date + 5.days
+    start_date = params[:start_date].to_datetime
+
+    @days = [0, 1, 2, 3, 4].map { |offset| start_date.advance(days: offset) }
     @tutors = Tutor.all
   end
 end
