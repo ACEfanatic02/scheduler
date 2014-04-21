31a8405745de8224486e8f20e7db3c0995e20185
ACEfanatic02 <acefanatic02@gmail.com>
2014-04-20 21:09:09 -0400

    Integration testing for the main schedule page.
    Testing content and behavior.  Lots of dynamic rendering going on here,
    need to ensure everything acts as it should.

---
+++ spec/features/schedule_pages_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe "Schedule pages" do
+  before do
+    @alice = User.create!(username: 'alice', email: 'alice@example.com',
+                          password: 'password', password_confirmation: 'password')
+    @bob = User.create!(username: 'bob', email: 'bob@example.com',
+                        password: 'password', password_confirmation: 'password')
+    @csc200 = Subject.create!(course_number: 'CSC200', course_name: 'Intro to Computer Science')
+    @eng111 = Subject.create!(course_number: 'ENG111', course_name: 'College Composition I')
+    @alice.build_tutor(subjects: [@csc200]).save!
+    @bob.build_tutor(subjects: [@eng111]).save!
+  end
+
+  describe "page contents" do
+    before do
+      visit schedule_path(start_date: DateTime.civil_from_format(:local, 2014, 3, 31))
+    end
+
+    it "has the right title" do
+      expect(page).to have_title("Monday March 31 - Friday April 4")
+    end
+
+    it "has tables for each weekday" do
+      ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"].each do |weekday|
+        expect(page).to have_css('.schedule-table-day-header', text: weekday)
+      end
+    end
+
+    it "does not have tables for the weekend" do
+      ["SATURDAY", "SUNDAY"].each do |weekend|
+        expect(page).to have_no_css('.schedule-table-day-header', text: weekend)
+      end
+    end
+
+    it "has a row of times for each day" do
+      expect(all('.schedule-table-time-header').count).to eq(5 * 10)
+    end
+
+    it "has a row for each tutor every day" do
+      # TODO:  Tutors with a completely blacked-out schedule for a particular day
+      # should *not* show up -- but we don't have blackouts implmented yet.
+      expect(all('.schedule-table-tutor-name', text: 'alice').count).to eq(5)
+      expect(all('.schedule-table-tutor-name', text: 'bob').count).to eq(5)
+    end
+  end
+end
