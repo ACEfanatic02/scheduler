046fcd11d4e193f3f1ff62c0612d75f13e5376c4
ACEfanatic02 <acefanatic02@gmail.com>
2014-04-27 17:39:22 -0400

    Schedule blocks link to new appointment page.
    Currently using JS hacks -- these will eventually turn into an AJAX
    call to pull in contents for a modal dialog -- maybe need to find a
    noscript solution as well?

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -3,8 +3,9 @@ $(function() {
   $('.schedule-table-schedule-block').click(function(e) {
     var time = $(this).data('time');
     var tutor = $(this).closest('tr').data('tutor-id');
+    var link = $(this).data('appt-link');
 
-    alert(tutor + " :: " + time);
+    window.location = link;
   });
 
 });
