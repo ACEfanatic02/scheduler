02a26761c4aca42d2272dad8164e9c3d75884fe3
ACEfanatic02 <acefanatic02@gmail.com>
2014-04-27 17:28:20 -0400

    Appointment form validation.
    Validate subject chosen, and notes exist for "Other" subject.

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,11 +1,42 @@
 $(function() {
 
+  /* Toggle 'required' mark on notes -- only required for a subject of 'Other'. */
   $('#appt-subject').change(function (e) {
     if ($(this).val() == "Other") {
       $('#appt-form-notes-required').removeClass('hidden');
     }
     else {
       $('#appt-form-notes-required').addClass('hidden');
+    }
+  });
+
+  function validateAppointmentForm() {
+    var subject_elem = $('#appt-subject');
+    var notes_elem = $('#appt-notes');
+    var valid = true;
+
+    // Clear previous errors.
+    subject_elem.removeClass('form-field-error');
+    notes_elem.removeClass('form-field-error');
+
+    if (!subject_elem.val()) {
+      subject_elem.addClass('form-field-error');
+      valid = false;
+    }
+
+    if (subject_elem.val() == "Other") {
+      if (!notes_elem.val()) {
+        notes_elem.addClass('form-field-error');
+        valid = false;
+      }
+    }
+    return valid;
+  }
+
+  $('#appt-button-submit').click(function (e) {
+    e.preventDefault();
+    if (validateAppointmentForm()) {
+      $('#new-appt-form').submit();
     }
   });
 
