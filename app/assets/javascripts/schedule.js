9fc79749409044199fd9c7a99ca781dd5b9051ba
ACEfanatic02 <acefanatic02@gmail.com>
2014-04-28 15:55:09 -0400

    Schedule block links.
    Use anchor links rather than an onclick listener on the table cells to
    link to a new appointment page.  This now works regardless of whether
    user has JavaScript enabled.

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -1,11 +1,3 @@
 $(function() {
 
-  $('.schedule-table-schedule-block').click(function(e) {
-    var time = $(this).data('time');
-    var tutor = $(this).closest('tr').data('tutor-id');
-    var link = $(this).data('appt-link');
-
-    window.location = link;
-  });
-
 });
